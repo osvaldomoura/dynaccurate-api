584138b1cd01041bb321e4f894bdc8fe65e2b49e
Osvaldo Moura <mouradeveloper@gmail.com>
2022-01-25 01:28:27 -0300

    inicio

---
+++ src/test/java/br/com/dynaccurate/dynaccurateapi/DynaccurateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dynaccurate.dynaccurateapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynaccurateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
